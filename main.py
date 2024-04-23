9d7e5c3728f2e095c2197f99c86f2d426c0528ea
iykyk <vicevasquez95@gmail.com>
2024-04-22 20:28:40 -0400

    Modify(import, db): matplot and db

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import numpy as np
 import pandas as pd
-import matplot as plt
+import matplotlib.pyplot as plt
 
-# maps = ['de_inferno' 'de_nuke' 'de_mirage' 'de_dust2']
-# matchs_id = data['MatchId'].unique() # 333 Games
-# count_maps = data['Map'].unique() # 4 Mapas
+db = 'https://raw.githubusercontent.com/razbackup/CSV-EV2/main/dbcs.csv'
+data = pd.read_csv(db, engine='python', sep=';', encoding='utf-8')
 
-
-data = pd.read_csv('./db/dbcs.csv', engine='python', sep=';', encoding='utf-8')
 matchs_maps = data[['MatchId', 'Map']] # Saco solo las columnas MatchId y Map 79157 rows
 unique_matchs_maps = matchs_maps.drop_duplicates() # Filtra los IDS dejandolos unicos 333 rows
 map_counts = unique_matchs_maps['Map'].value_counts().reset_index() # Cuenta por ID unico de Mapas un tipo de grupo de 4 y cuenta cuantas veces cae el ID en relacion al mapa
@@ -24,17 +22,5 @@ plt.title('Total Played per Map')
 # Rota las etiquetas del eje x para mayor legibilidad
 plt.xticks(rotation=90)
 
-# Muestra el gráfico
+# guardar la imagen en un archivo
 plt.show()
-# print(map_counts)
-
-# print(map_counts['Total_Played'].sum())
-
-
-
-
-
-    
-
-
-
