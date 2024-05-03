5a2fa728fb593f5feb46655d8b941fbd6b888798
raz <vicevasquez95@gmail.com>
2024-05-03 16:43:54 -0400

    trying to get wins of team by maps

--- functions.py
+++ functions.py
@@ -5,7 +5,6 @@ def findMatchs(data):
     lista = arr.tolist() # Convierte a lista  
     return lista
 
-
 def findHalfsByMatch(data, match_id):
     match = data[data['MatchId'] == match_id]
     half_one = match[match['RoundId'] <= 15]
@@ -27,7 +26,6 @@ def count_round_winner(team_df, team_name):
 def findWinnersByHalf(half):
     halfOne = {}
     halfTwo = {}
-    
     for half_df, half_name in zip(half, ["HalfOne", "HalfTwo"]):
         for team_name in ["Terrorist", "CounterTerrorist"]:
             win_count = count_round_winner(half_df, team_name)
@@ -37,3 +35,48 @@ def findWinnersByHalf(half):
                 halfTwo[team_name] = win_count
     
     return [halfOne, halfTwo]
+
+def findMapByMatch(match):
+    return match['Map'].loc[0]
+
+def findMapsByData(data):
+    return list(data['Map'].drop_duplicates())
+
+def findRoundByMatch(match):
+    return list(match["RoundId"].drop_duplicates())
+
+def findWinnerByMap(match_id, data):    
+    match = data[data['MatchId'] == match_id]
+    map_ = findMapByMatch(match)
+    rounds = findRoundByMatch(match=match)
+    print(f"got rounds {len(rounds)}")
+    
+    T = 0
+    CT = 0
+    
+    print("round for getting")
+    for i in rounds:
+        matchByRound = match[match["RoundId"] == i]
+        matchByRoundByT = matchByRound[matchByRound["Team"] == "Terrorist"] 
+        matchByRoundByCT = matchByRound[matchByRound["Team"] == "CounterTerrorist"]
+        print("Filttered Match By Round Teams")
+        
+        winT = matchByRoundByT["RoundWinner"].drop_duplicates()
+        winCT = matchByRoundByCT["RoundWinner"].drop_duplicates()
+        
+        print(winT)
+        print(winCT)
+        
+        # print("Getting bools")
+        # if winT == True:
+        #     T += 1
+        # if winCT == True:
+        #     CT += 1
+    
+    RoundWinners = {
+        "Terorist": T,
+        "CounterTerrorist": CT
+    }
+    
+    return RoundWinners
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functions as f
+from functions import *
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,58 +7,6 @@ import matplotlib.pyplot as plt
 db = 'https://raw.githubusercontent.com/razbackup/CSV-EV2/main/dbcs.csv'
 data = pd.read_csv(db, engine='python', sep=';', encoding='utf-8')
 
-# Encontrar los datos de Terrorist y CounterTerrorist para cada mitad
-matchs = f.findMatchs(data)
-T_arr_half1 = []
-CT_arr_half1 = []
-T_arr_half2 = []
-CT_arr_half2 = []
 
-for match in matchs:
-    halfs = f.findHalfsByMatch(data, match)
-    half_one, half_two = f.findWinnersByHalf(halfs)
-    T_arr_half1.append(half_one['Terrorist'])
-    CT_arr_half1.append(half_one['CounterTerrorist'])
-    T_arr_half2.append(half_two['Terrorist'])
-    CT_arr_half2.append(half_two['CounterTerrorist'])
 
-# Agrupar los datos en intervalos de 10 partidas y calcular el promedio
-def group_and_average(data):
-    grouped_data = []
-    for i in range(0, len(data), 10):
-        group = data[i:i+10]
-        avg = sum(group) / len(group)
-        grouped_data.append(avg)
-    return grouped_data
-
-T_arr_half1_grouped = group_and_average(T_arr_half1)
-CT_arr_half1_grouped = group_and_average(CT_arr_half1)
-T_arr_half2_grouped = group_and_average(T_arr_half2)
-CT_arr_half2_grouped = group_and_average(CT_arr_half2)
-
-# Crear los gráficos
-plt.figure(figsize=(12, 6))
-
-# Primer gráfico para la primera mitad
-plt.subplot(1, 2, 1)
-plt.plot(range(0, len(T_arr_half1_grouped) * 10, 10), T_arr_half1_grouped, label='Terrorist')
-plt.plot(range(0, len(CT_arr_half1_grouped) * 10, 10), CT_arr_half1_grouped, label='CounterTerrorist')
-plt.xlabel('Partidos')
-plt.ylabel('Promedio de Rondas Ganadas')
-plt.title('Primera Mitad (Cada 10 partidos)')
-plt.legend()
-
-# Segundo gráfico para la segunda mitad
-plt.subplot(1, 2, 2)
-plt.plot(range(0, len(T_arr_half2_grouped) * 10, 10), T_arr_half2_grouped, label='Terrorist')
-plt.plot(range(0, len(CT_arr_half2_grouped) * 10, 10), CT_arr_half2_grouped, label='CounterTerrorist')
-plt.xlabel('Partidos')
-plt.ylabel('Promedio de Rondas Ganadas')
-plt.title('Segunda Mitad (Cada 10 partidos)')
-plt.legend()
-
-# Ajustar el espacio entre los gráficos
-plt.tight_layout()
-
-# Mostrar los gráficos
-plt.show()
+print(findWinnerByMap(4, data))
